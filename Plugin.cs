8ae2de22fb30bde86196f60bf9c5bdcd48827a17
cultab <rroarck@gmail.com>
2023-12-10 22:40:49 +0200

    init

---
+++ Plugin.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using TerminalApi;
+using static TerminalApi.TerminalApi;
+
+namespace LethalFetch
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+	[BepInDependency("atomic.terminalapi", MinimumDependencyVersion: "1.2.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            var text = ""OS: FORTUNE OS
+Kernel: v0.04
+Uptime: ???
+Packages: 5 (apt)
+Shell: quota
+Terminal: VT-10
+CPU: BORSON 300 @ 2500 MHz
+GPU: N/A
+Memory:  12 MiB / 431 MiB
+"";
+            AddCommand("neofetch", text);
+        }
+    }
+}
